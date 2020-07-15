30f6104d15ee293b6774da3fa93fd7766cc48a9a
moasko <42101671+moasko@users.noreply.github.com>
2020-07-15 01:24:13 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 const puce = document.querySelector('.puce')
 const navigation = document.querySelector('.navigation')
 const navlink = document.querySelectorAll('.navlink')
+const puces = document.querySelectorAll('.puce>div')
+
+
 
 let voir = () => {
     navigation.classList.toggle('vue')
     navigation.classList.toggle('shadowb')
 }
 
-puce.onclick = voir
+let colorchage = () => {
+    puces.forEach(r => {
+        r.classList.toggle('back')
+    })
+}
+
+puce.onclick = () => {
+    voir()
+    colorchage()
+}
+
 navlink.forEach(e => {
     e.onclick = voir
 })
